77aa0a91710e1154a47f54ba2cedad5896a50c6e
atul3940 <atul7784@gmail.com>
2017-03-02 16:38:46 +0530

    first commit

---
+++ snippets/serializers.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 1, 2017
+
+@author: atulgupta
+'''
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
